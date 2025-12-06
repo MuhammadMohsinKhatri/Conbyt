9b9c979aacf4fe263a7fd11cb476a165a7682b67
muhammadmohsin <muhmmadmohsinkhatri@gmail.com>
2025-12-07 04:44:47 +0500

    remove fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
@@ -21,40 +21,49 @@ import Payments from "./pages/CMS/Payments";
 import Portfolios from "./pages/CMS/Portfolios";
 
 
+function AppContent() {
+  const location = useLocation();
+  const isCMSRoute = location.pathname.startsWith('/cms/');
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      {!isCMSRoute && <Header />}
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/case-studies" element={<CaseStudies />} />
+          <Route path="/case-study/:slug" element={<CaseStudyDetail />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<BlogDetail />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-of-service" element={<TermsOfService />} />
+          {/* CMS Routes */}
+          <Route path="/cms/login" element={<CMSLogin />} />
+          <Route path="/cms/dashboard" element={<CMSDashboard />} />
+          <Route path="/cms/blogs" element={<Blogs />} />
+          <Route path="/cms/blogs/new" element={<BlogEditor />} />
+          <Route path="/cms/blogs/edit/:id" element={<BlogEditor />} />
+          <Route path="/cms/contact" element={<ContactSubmissions />} />
+          <Route path="/cms/clients" element={<Clients />} />
+          <Route path="/cms/clients/new" element={<Clients />} />
+          <Route path="/cms/projects" element={<Projects />} />
+          <Route path="/cms/projects/new" element={<Projects />} />
+          <Route path="/cms/milestones" element={<Milestones />} />
+          <Route path="/cms/payments" element={<Payments />} />
+          <Route path="/cms/portfolios" element={<Portfolios />} />
+          <Route path="/cms/portfolios/new" element={<Portfolios />} />
+        </Routes>
+      </main>
+      {!isCMSRoute && <Footer />}
+      <ScrollToTop />
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/case-studies" element={<CaseStudies />} />
-            <Route path="/case-study/:slug" element={<CaseStudyDetail />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/blog/:slug" element={<BlogDetail />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/terms-of-service" element={<TermsOfService />} />
-            {/* CMS Routes */}
-            <Route path="/cms/login" element={<CMSLogin />} />
-            <Route path="/cms/dashboard" element={<CMSDashboard />} />
-            <Route path="/cms/blogs" element={<Blogs />} />
-            <Route path="/cms/blogs/new" element={<BlogEditor />} />
-            <Route path="/cms/blogs/edit/:id" element={<BlogEditor />} />
-            <Route path="/cms/contact" element={<ContactSubmissions />} />
-            <Route path="/cms/clients" element={<Clients />} />
-            <Route path="/cms/clients/new" element={<Clients />} />
-            <Route path="/cms/projects" element={<Projects />} />
-            <Route path="/cms/projects/new" element={<Projects />} />
-            <Route path="/cms/milestones" element={<Milestones />} />
-            <Route path="/cms/payments" element={<Payments />} />
-            <Route path="/cms/portfolios" element={<Portfolios />} />
-            <Route path="/cms/portfolios/new" element={<Portfolios />} />
-          </Routes>
-        </main>
-        <Footer />
-        <ScrollToTop />
-      </div>
+      <AppContent />
     </Router>
   );
 }
