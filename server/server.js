eb57fc507752b9aed29d81847ce5495a529cb869
muhammadmohsin <muhmmadmohsinkhatri@gmail.com>
2025-12-08 14:05:06 +0500

    fix

--- server/server.js
+++ server/server.js
@@ -40,8 +40,8 @@ const corsOptions = {
   credentials: true
 };
 app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Routes
 app.use('/api/case-studies', caseStudiesRoutes);
